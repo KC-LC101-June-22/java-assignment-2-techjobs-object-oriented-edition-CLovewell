e2b955aee5b69ffc088c5b12e5b31b6cbdd88260
Carnahan Lovewell <carnahan.lovewell@gmail.com>
2022-10-16 19:53:11 -0500

    Created abstract class JobField and extended it to each job field class.

--- src/main/java/org/launchcode/techjobs/oo/Location.java
+++ src/main/java/org/launchcode/techjobs/oo/Location.java
@@ -2,11 +2,8 @@ package org.launchcode.techjobs.oo;
 
 import java.util.Objects;
 
-public class Location {
-
-    private int id;
+public class Location extends JobField {
     private static int nextId = 1;
-    private String value;
 
     public Location() {
         id = nextId;
@@ -18,13 +15,6 @@ public class Location {
         this.value = value;
     }
 
-    // Custom toString, equals, and hashCode methods:
-
-    @Override
-    public String toString() {
-        return value;
-    }
-
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -32,24 +22,4 @@ public class Location {
         Location location = (Location) o;
         return getId() == location.getId();
     }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(getId());
-    }
-
-    // Getters and Setters:
-
-    public int getId() {
-        return id;
-    }
-
-    public String getValue() {
-        return value;
-    }
-
-    public void setValue(String value) {
-        this.value = value;
-    }
-
 }
--- src/main/java/org/launchcode/techjobs/oo/PositionType.java
+++ src/main/java/org/launchcode/techjobs/oo/PositionType.java
@@ -2,11 +2,8 @@ package org.launchcode.techjobs.oo;
 
 import java.util.Objects;
 
-public class PositionType {
-
-    private int id;
+public class PositionType extends JobField{
     private static int nextId = 1;
-    private String value;
 
     public PositionType() {
         id = nextId;
@@ -19,35 +16,10 @@ public class PositionType {
     }
 
     @Override
-    public String toString() {
-        return value;
-    }
-
-    @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         PositionType that = (PositionType) o;
         return id == that.id;
     }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
-
-    // Getters and Setters:
-
-    public int getId() {
-        return id;
-    }
-
-    public String getValue() {
-        return value;
-    }
-
-    public void setValue(String value) {
-        this.value = value;
-    }
-
 }
